96093b4b38e1f34f3c24537dc4bc136817da56ae
Jason Harrison <jasonharrisonvancouver@gmail.com>
2019-01-11 16:16:51 -0800

    Initial Commit

---
+++ PigLatin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PigLatin
+//
+//  Created by jason harrison on 2019-01-11.
+//  Copyright © 2019 jason harrison. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
